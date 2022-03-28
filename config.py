2b2341ba0a4016500a2a4b3c69ed6221abd6230b
m7hmd <102188298+m7hmd@users.noreply.github.com>
2022-03-28 10:24:11 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "5245849897:AAGm3VpWKSKF1bPsk6XKmiBZlhpnBpAW7hY"
+BOT_TOKEN = "679514152:AAEu8pKRxv1piPEdSKEoR8deN3TwNnCWqaM"
 APP_URL = "https://midddddddbot.herokuapp.com/" + BOT_TOKEN
