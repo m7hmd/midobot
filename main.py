4efb111e2c02773d0ef4fc6aa50c0f822cfb077c
m7hmd <102188298+m7hmd@users.noreply.github.com>
2022-04-11 15:45:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import logging
 from config import *
 from flask import Flask, request
 
-BOT_TOKEN = "679514152:AAEu8pKRxv1piPEdSKEoR8deN3TwNnCWqaM"
+BOT_TOKEN = "5133056238:AAGluF9S2vYrxmVkSjykww0KUOf7B6dSB3c"
 bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
 logger = telebot.logger
@@ -14,15 +14,25 @@ logger.setLevel(logging.DEBUG)
 w = ['1', "2","3","4",'5', "6","7","8",'9', "0","q","w",'e', "r","t","y",'u', "i","o","p",'a', "s","d","f","g","h",'j', "k","l","z",'x', "c","v","b",'n', "m"]
 @bot.message_handler(content_types=["text"])
 def gen(message):
-    if "*" in message.text:
-        word = message.text
-        m = ''
-        a = []
-        for i in w:
-            a.append(word.replace("*", i) + "\n")
-        bot.send_message(message.chat.id, m.join(a))
+    if "/" not in message.text:
+        z = message.text[0]
+        n = len(message.text)
+        n2 = n - 1
+        lists = []
+        for n in range(n):
+            list = []
+            for i in range(n2):
+                (list.append(z))
+            list.insert(n, '*')
+            lists.append("".join(list))
+        for word in lists:
+            m = ''
+            a = []
+            for i in w:
+                a.append(word.replace("*", i) + "\n")
+            bot.send_message(message.chat.id, m.join(a))
     else:
-        bot.send_message(message.chat.id, "Send Test Like : z*zzz")
+        bot.send_message(message.chat.id, "Send Test Like : zzzz")
    
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
@@ -34,5 +44,5 @@ def redirect_message():
 
 if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url="https://telebobt.herokuapp.com/" + str(BOT_TOKEN))
+    bot.set_webhook(url="https://listmakerfz.herokuapp.com/" + str(BOT_TOKEN))
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
